5da27ec72588d6d5d1c9a447f905271892be6478
Colin <marshallcolin71@gmail.com>
2018-05-21 13:46:42 -0400

    redirect to list after item creation + routes update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,14 @@
 class ItemsController < ApplicationController
 
   def create
+    @user = User.find(params[:user_id])
     @item = Item.new(item_params)
     @item.user = current_user
     @new_item = Item.new
 
     if @item.save
       flash[:notice] = "To-Do list has been successfully updated"
+      redirect_to [@user]
     else
       flash.now[:alert] = "Sorry, there was an error updating. Please try again"
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
 
   def show
     @item = Item.new
-    @items = Item.where(@user == current_user)
+    @items = current_user.items
   end
 end
