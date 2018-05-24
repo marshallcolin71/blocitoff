9e9b107dc99f64b5b63a761e69a88113f48fb847
Colin <marshallcolin71@gmail.com>
2018-05-23 22:00:18 -0400

    added onto seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,31 @@
+require 'faker'
+
+15.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker:Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+
 10.times do
   Item.create!(
+    user: users.sample,
     name: Faker::Lorem.sentence,
   )
 end
 
+items = Item.all
+
 User.create!(
   email: 'marshallcolin71@gmail.com',
   password: 'password'
+
+  user.skip_confirmation!
 )
 
 puts "seed finished"
