49e410d487c48cfce9e84e1301801b19fc35587f
Colin <marshallcolin71@gmail.com>
2018-06-01 20:51:18 -0400

    added blocmetrics javascript snippet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,13 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+var blocmetrics = {};
+blocmetrics.report = function(eventName){
+  var event = {event: { name: eventName }};
+
+  // #2
+  var request = new XMLHttpRequest();
+  // #3
+  request.open("POST", "http://localhost:3000/api/events", true);
+  // #4
+  request.setRequestHeader('Content-Type', 'application/json');
+  // #5
+  request.send(JSON.stringify(event));
+ };
